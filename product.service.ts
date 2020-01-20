28398c54118efab41c201aa1d4f156aa5b38829f
OngenaePieter <59998987+OngenaePieter@users.noreply.github.com>
2020-01-20 11:30:59 +0100

    product geupdate

--- product.service.ts
+++ product.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product';
+import { concat } from 'rxjs';
 
 @Injectable()
 export class ProductService {
-   getAllProducts(): any {
-      let Product1 = "name: Belgin 13, brand: Belgin,description: Belgin Ultra 13, a new Belgian gin, distilled according to old principles. The Belgin Ultra 13 has an own personality, a very nice gin with a complicated taste. ,price: 28.99"
-      let Product2 = "name: Tanqueray Nr10,brand: Tanqueray,description: Just like the Tanqueray Special Dry, the Nr10 has an alcohol percentage of about 47,30 %. This gin as introduced for the first time in 2000 and is a nice addition to the portfolio of this producer.,price: 40.99"
-      return Product1 + Product2;
+   getAllProducts(): Product[] {
+      const product1 = new Product("Belgin", "Belgin", " Belgin Ultra 13, a new Belgian gin, distilled according to old principles.", 28.99)
+      const product2 = new Product("Tanqueray Nr10", "Tanqueray", " Just like the Tanqueray Special Dry, the Nr10 has an alcohol percentage of about 47,30", 40.99)
+
+      return [product1, product2];
    }
 
    constructor(
